312b08824e8645a79debe2772c655d2c9f59fba5
sadhanadondeti1 <sadhana.dondeti@eagles.oc.edu>
2022-07-04 12:28:35 -0500

    create survey find survey added

---
+++ app/routes/survey.routes.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+    const survey = require("../controllers/survey.controller.js");
+    
+    var router = require("express").Router();
+    // Create a new Tutorial
+    router.post("/", survey.create);
+    // Retrieve all Tutorials
+   // Retrieve a single Tutorial with id
+  router.get("/:id", survey.findOne);
+   // Retrieve all Tutorials
+   router.get("/", survey.findAll);
+    app.use('/api/surveys+', router);
+  };
