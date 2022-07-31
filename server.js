7f6007308b3329d0f3e15865f8b1c6076c1aea2f
venkatesh766 <venkatesh.arla@eagles.oc.edu>
2022-07-31 00:11:07 -0500

    unit test cases for survey controller

--- server.js
+++ server.js
@@ -37,10 +37,11 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/survey.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+if (process.env.NODE_ENV !== 'test') {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
+}
 function initial() {
   Role.create({
     id: 1,
